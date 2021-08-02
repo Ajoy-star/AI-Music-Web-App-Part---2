0bf76f00b2a1bfd1bee1896b30f66406393e0e57
Ajoy-star <83897482+Ajoy-star@users.noreply.github.com>
2021-08-02 16:55:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
-function preload()
-{
-    song = loadSound("music1.mp3,music2.mp3");
-}
-
-
-function setup()
-{
+function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
  
     video = createCapture(VIDEO);
     video.hide();
-}
-
-function draw()
-{
-     image(video, 0, 0, 600, 500);
+ }
+ function draw() {
+    image(video, 0, 0, 600, 500);
 }
